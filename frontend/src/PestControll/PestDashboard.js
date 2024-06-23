99acba858537115856df055301a60113159f2597
IT21042560 <it21042560@my.sliit.lk>
2024-06-23 13:03:14 +0530

    Pest dashboard updated

--- frontend/src/PestControll/PestDashboard.js
+++ frontend/src/PestControll/PestDashboard.js
@@ -5,25 +5,167 @@ import {
   TouchableOpacity,
   Image,
   Alert,
+  ScrollView,
 } from "react-native";
 import React, { useState, useRef } from "react";
 import Header from "../Screen/Header/Index";
 import Footer from "../Screen/Footer/Index";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function PestDashboard(props) {
   return (
-    <View
-     style={{
-        backgroundColor: "white",
-        height: "100%",
-        width: "100%",
-        paddingTop: 30,
-      }}>
-    <Header />
-      
-    <Footer />
+    <View style={styles.mainContainer}>
+      <Header />
+      <View>
+        <ScrollView>
+          <Image
+            style={{ width: "100%", height: 180 }}
+            source={require("../Screen/assets/pest_cover.png")}
+          />
+
+          <View style={{ paddingTop: 20 }}>
+            <View style={{ justifyContent: "center" }}>
+              <Text style={styles.subHedding}>The Most Common Pests</Text>
+            </View>
+
+            <View style={styles.imageGridContainer1}>
+              <View style={styles.imageGridRow}>
+                <View>
+                  <Image
+                    source={require("./assets/pest_1.png")}
+                    style={{
+                      width: 80,
+                      height: 90,
+                      marginLeft: 20,
+                      marginRight: 0,
+                    }}
+                  />
+                  <Text style={styles.gridText}>Aphids</Text>
+                </View>
+
+                <View>
+                  <Image
+                    source={require("./assets/pest_2.png")}
+                    style={{
+                      width: 80,
+                      height: 90,
+                      marginLeft: 30,
+                      marginRight: 30,
+                    }}
+                  />
+                  <Text style={styles.gridText}>Mites</Text>
+                </View>
+                <View>
+                  <Image
+                    source={require("./assets/pest_3.png")}
+                    style={styles.image}
+                  />
+                  <Text style={{ color: "#6a859f", fontWeight: "bold" }}>
+                    Catepiller
+                  </Text>
+                </View>
+              </View>
+
+              <View style={styles.imageGridRow}>
+                <View>
+                  <Image
+                    source={require("./assets/pest_4.png")}
+                    style={styles.image}
+                  />
+                  <Text style={{ color: "#6a859f", fontWeight: "bold", marginLeft:15 }}>
+                    Thrips
+                  </Text>
+                </View>
+                <View>
+                  <Image
+                    source={require("./assets/pest_5.png")}
+                    style={styles.image}
+                  />
+                  <Text style={styles.gridText}>
+                    Whiteflies
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          {/* <View style={{ paddingTop: 10 }}>
+            <View
+              style={{
+                backgroundColor: "#2f4f4f",
+                height: 100,
+                width: "90%",
+                alignSelf: "center",
+                paddingTop:10
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "#004242",
+                  height: 'auto',
+                  alignSelf: "center",
+                  width: "90%",
+                  paddingTop: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <FontAwesome
+                  name="lightbulb-o"
+                  size={30}
+                  color={"white"}
+                  style={{
+                    borderRadius: 100,
+                    padding:2,
+                    
+                  }}
+                />
+                <Text style={{fontSize:18, color:'#68e8cb', fontWeight:'bold'}}>Innovation & sustainability are our commitment</Text>
+              </View>
+            </View>
+          </View> */}
+        </ScrollView>
+      </View>
+      <Footer />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: "white",
+    height: "100%",
+    width: "100%",
+    paddingTop: 30,
+  },
+  subHedding: {
+    textAlign: "center",
+    fontSize: 19,
+    fontWeight: "bold",
+    color: "#061f1e",
+  },
+  image: {
+    width: 80,
+    height: 90,
+    marginLeft: 0,
+    marginRight: 20,
+  },
+  imageGridContainer1: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+  },
+  imageGridRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+    paddingTop: 10,
+  },
+  gridText: {
+    textAlign: "center",
+    color: "#6a859f",
+    fontWeight: "bold",
+  },
+});
--- frontend/src/Screen/Home/Home.js
+++ frontend/src/Screen/Home/Home.js
@@ -1,30 +1,19 @@
-import { StyleSheet, Text, View, Button, PixelRatio } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  ScrollView,
+} from "react-native";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "../../Context/AuthContext";
-//import { useVideoPlayer, VideoView } from "expo-video";
 import Header from "../Header/Index";
 import Footer from "../Footer/Index";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const videoSource = require("../assets/home.mp4");
 const Home = (props) => {
   const { UserLogOut } = useContext(AuthContext);
-
-  // const ref = useRef(null);
-  // const [isPlaying, setIsPlaying] = useState(true);
-  // const player = useVideoPlayer(videoSource, (player) => {
-  //   player.loop = true;
-  //   player.play();
-  // });
-
-  // useEffect(() => {
-  //   const subscription = player.addListener("playingChange", (isPlaying) => {
-  //     setIsPlaying(isPlaying);
-  //   });
-
-  //   return () => {
-  //     subscription.remove();
-  //   };
-  // }, [player]);
 
   return (
     <View
@@ -36,16 +25,50 @@ const Home = (props) => {
       }}
     >
       <Header />
-      {/* <VideoView
-        ref={ref}
-        style={{
-          width: 350,
-          height: 275,
-        }}
-        player={player}
-        allowsFullscreen
-        allowsPictureInPicture
-      /> */}
+      <View>
+        <ScrollView>
+          <Image
+            style={{ width: "100%", height: 180 }}
+            source={require("../assets/pest_cover.png")}
+          />
+
+          <View style={{ paddingTop: 10 }}>
+            <View
+              style={{
+                backgroundColor: "#2f4f4f",
+                height: 100,
+                width: "90%",
+                alignSelf: "center",
+                paddingTop:10
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "#004242",
+                  height: 'auto',
+                  alignSelf: "center",
+                  width: "90%",
+                  paddingTop: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <FontAwesome
+                  name="lightbulb-o"
+                  size={30}
+                  color={"white"}
+                  style={{
+                    borderRadius: 100,
+                    padding:2,
+                    
+                  }}
+                />
+                <Text style={{fontSize:18, color:'#68e8cb', fontWeight:'bold'}}>Innovation & sustainability are our commitment</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
       <Footer />
     </View>
   );
@@ -63,6 +86,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     fontSize: 24,
     paddingTop: 10,
+  },
+  backgroundVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
 
