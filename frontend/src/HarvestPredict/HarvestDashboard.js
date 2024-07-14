846d45357f8ad3bb388a034151cca584150b96a7
Amalya <it20254216@my.sliit.lk>
2024-07-15 00:00:18 +0530

    Harvest dashboard completed

--- frontend/src/HarvestPredict/HarvestDashboard.js
+++ frontend/src/HarvestPredict/HarvestDashboard.js
@@ -46,6 +46,7 @@ export default function HarvestDashboard(props) {
                   alignItems: "center",
                 }}
               >
+                
                 <FontAwesome
                   name="lightbulb-o"
                   size={30}
@@ -54,8 +55,8 @@ export default function HarvestDashboard(props) {
                     padding: 10,
                   }}
                 />
-                <Text style={styles.subHedding}>Harvest Smarter
-                  Predict, Analyze & Cultivate.
+                <Text style={styles.subHedding}>
+                  Harvest Smarter, Predict, Analyze & Cultivate.
                 </Text>
                 <View>
                   <Text
@@ -145,8 +146,8 @@ export default function HarvestDashboard(props) {
                 /> */}
 
               <Image
-                source={require("./assets/harvest dashboard.jpeg")}
-                style={{ width: "100%", height: 250 }}
+                source={require("./assets/fertilizer1.jpeg")}
+                style={{ width: "100%", height: 150 }}
               />
 
               <View
@@ -160,7 +161,7 @@ export default function HarvestDashboard(props) {
                 }}
               >
                 <FontAwesome
-                  name="cube"
+                  name="slack"
                   size={25}
                   color={"#33705b"}
                   style={{
@@ -174,94 +175,24 @@ export default function HarvestDashboard(props) {
                     letterSpacing: -0.5,
                   }}
                 >
-                  With our augmented reality (AR) feature, you can view and
-                  experience a 3D model of the identified pest right in your
-                  field. This immersive experience will help you understand the
-                  pest's characteristics and behavior better.
+                  With our advanced factor analysis feature, you can identify
+                  the key factors influencing harvest predictions. This powerful
+                  tool provides actionable suggestions to align actual harvests
+                  with expected values, helping you to avoid deficiencies or
+                  excesses. Understanding these critical factors enables better
+                  decision-making and ensures optimal crop yields.
                 </Text>
 
-                <View style={{ paddingTop: 20 }}>
+                <View style={{ padding: 20 }}>
                   <Button
-                    title="AR View"
-                    onPress={() => navigation.navigate("OpenPestCamara")}
+                    title="Factor Analysis"
+                    onPress={() => navigation.navigate(" ")}
                     color={"#33705b"}
                   />
                 </View>
               </View>
             </View>
           </View>
-
-          <View style={{ paddingTop: 30 }}>
-            <View style={{ justifyContent: "center" }}>
-              <Text style={styles.subHedding}>The Most Common Pests</Text>
-            </View>
-
-            <View style={styles.imageGridContainer1}>
-              <View style={styles.imageGridRow}>
-                <View>
-                  {/*<Image
-                      source={require("./assets/pest_1.png")}
-                      style={{
-                        width: 80,
-                        height: 90,
-                        marginLeft: 20,
-                        marginRight: 0,
-                      }}
-                    /> */}
-                  <Text style={styles.gridText}>Aphids</Text>
-                </View>
-
-                <View>
-                  {/*<Image
-                      source={require("./assets/pest_2.png")}
-                      style={{
-                        width: 80,
-                        height: 90,
-                        marginLeft: 30,
-                        marginRight: 30,
-                      }}
-                    />*/}
-                  <Text style={styles.gridText}>Mites</Text>
-                </View>
-                <View>
-                  {/*<Image
-                      source={require("./assets/pest_3.png")}
-                      style={styles.image}
-                    />*/}
-                  <Text style={{ color: "#6a859f", fontWeight: "bold" }}>
-                    Catepiller
-                  </Text>
-                </View>
-              </View>
-
-              <View style={styles.imageGridRow}>
-                <View>
-                  {/* <Image
-                      source={require("./assets/pest_4.png")}
-                      style={styles.image}
-                    /> */}
-                  <Text
-                    style={{
-                      color: "#6a859f",
-                      fontWeight: "bold",
-                      marginLeft: 15,
-                    }}
-                  >
-                    Thrips
-                  </Text>
-                </View>
-                <View>
-                  {/* <Image
-                      source={require("./assets/pest_5.png")}
-                      style={styles.image}
-                    /> */}
-                  <Text style={styles.gridText}>Whiteflies</Text>
-                </View>
-              </View>
-            </View>
-          </View>
-
-          <View></View>
         </ScrollView>
       </View>
       <Footer />
